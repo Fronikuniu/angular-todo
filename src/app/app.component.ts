0d452abe80ba9090dc6975169765823330d77682
Fronikuniu <Fronikuniu@gmail.com>
2022-05-19 09:03:25 +0200

    Add array of tasks, addTask, removeTask and clearTasks function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/types';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'TODO LIST';
-  copyright = `${new Date().getFullYear()} © TODO List build with Angular.`;
+  config: Record<string, string> | null = null;
+  tasks: Task[] = [
+    { id: 1, name: 'Task 1', deadline: '2022-05-01', done: false },
+    { id: 2, name: 'Task 2', deadline: '2022-05-18', done: false },
+    { id: 3, name: 'Task 3', deadline: '2022-05-19', done: true },
+    { id: 4, name: 'Task 4', deadline: '2022-05-05', done: false },
+  ];
+
+  constructor() {
+    this.config = {
+      title: 'TODO LIST',
+      copyright: `${new Date().getFullYear()} © TODO List build with Angular.`,
+    };
+  }
+
+  addTask(name: string, deadline: string) {
+    const lastTask = this.tasks[this.tasks.length - 1];
+    if (name && deadline) {
+      this.tasks.push({
+        id: lastTask.id + 1,
+        name,
+        deadline,
+        done: false,
+      });
+    }
+  }
+
+  removeTask(id: number) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
 }
